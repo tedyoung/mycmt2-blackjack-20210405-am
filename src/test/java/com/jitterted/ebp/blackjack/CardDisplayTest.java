effb7bb41f51b066a79c3feda67d76f696bd7468
Ted M. Young <tedyoung@gmail.com>
2021-04-05 10:30:14 -0700

    Lab #1: Non-TEN Card display.

--- src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -13,4 +13,12 @@ public class CardDisplayTest {
     assertThat(tenCard.display())
         .isEqualTo("[30m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
   }
+
+  @Test
+  public void displayNonTenCard() throws Exception {
+    Card threeCard = new Card(Suit.DIAMONDS, Rank.THREE);
+
+    assertThat(threeCard.display())
+        .isEqualTo("[31m┌─────────┐[1B[11D│3        │[1B[11D│         │[1B[11D│    ♦    │[1B[11D│         │[1B[11D│        3│[1B[11D└─────────┘");
+  }
 }
